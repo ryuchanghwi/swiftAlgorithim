52f35d01f62445720d5d97db209bb3551b20d130
ryuchanghwi <ckdgnl10@naver.com>
2022-11-01 22:37:04 +0900

    외계행성의 나이 문제풀이 완료

--- AlgorithmSwift/AlgorithmSwift/main.swift
+++ AlgorithmSwift/AlgorithmSwift/main.swift
@@ -4,5 +4,20 @@
 //
 //  Created by 류창휘 on 2022/10/08.
 //
-
+//외계행성의 나이
 import Foundation
+//func solution(_ age:Int) -> String {
+//    var result = ""
+//    let intTuple = [("0", "a"), ("1", "b"), ("2", "c"), ("3", "d"),("4", "e"), ("5", "f"), ("6", "g"),("7", "h"), ("8", "i"), ("9", "j")]
+//    for i in Array(String(age)) {
+//        for j in intTuple {
+//            if String(i) == j.0 {
+//                result += j.1
+//            }
+//        }
+//    }
+//    return result
+//}
+func solution(_ age:Int) -> String {
+    return ""
+}
