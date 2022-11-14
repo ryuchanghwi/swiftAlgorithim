481242cff8ea73dbac6a76cf3df3c8b60a6798ff
ryuchanghwi <ckdgnl10@naver.com>
2022-11-15 02:56:33 +0900

    11.15 수박수박수? 풀이 완료

---
+++ AlgorithmSwift/AlgorithmSwift/Programmers/LV.1/수박수박수박수박수박수?.swift
@@ -0,0 +1,15 @@
+//
+//  수박수박수박수박수박수?.swift
+//  AlgorithmSwift
+//
+//  Created by 류창휘 on 2022/11/15.
+//
+
+import Foundation
+/*
+ 수박수박수박수박수박?
+ */
+func solution(_ n:Int) -> String {
+    let watermelonCount = n / 2
+    return n == 1 ? "수" : n > 1 && n % 2 == 1 ? String(repeating: "수박", count: watermelonCount)+"수" : String(repeating: "수박", count: watermelonCount)
+}
