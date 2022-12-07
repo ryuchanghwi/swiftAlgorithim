3dba7acf5a1332c2154068e3215f20a1d43238cf
ryuchanghwi <ckdgnl10@naver.com>
2022-12-08 00:37:31 +0900

    12/8 가운데 글자 가져오기

--- AlgorithmSwift/AlgorithmSwift/main.swift
+++ AlgorithmSwift/AlgorithmSwift/main.swift
@@ -137,32 +137,6 @@ import Foundation
 
 
 
-
-
-
-/*
- 1. 가운데 글자 가져오기
- */
-//func solution(_ s:String) -> String {
-//    let sCount = s.count
-//    // 5일떄는 2, 7일떄는 3... s / 2 인덱스?
-//    // 4일떄는 1,2 6일때는 2,3 8일떄는 34 s / 2 -1, s/2
-//    let sArray = s.map { value in
-//        String(value)
-//    }
-//    return sCount % 2 == 0 ? sArray[sCount / 2 - 1]+sArray[sCount / 2] : sArray[sCount / 2]
-//}
-//다른 사람 풀이
-//func solution(_ s:String) -> String {
-//    if Array(s).count % 2 == 0 {
-//        return String(Array(s)[(s.count/2)-1...(s.count/2)])
-//    }else{
-//        return String(Array(s)[s.count/2])
-//    }
-//}
-
-
-
 /*
  정수 내림차순으로 배치하기
  */
